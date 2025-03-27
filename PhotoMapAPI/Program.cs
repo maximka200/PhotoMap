7c24b034d5d7ca022b4ff4cdbb724fc194f800b7
maximka200 <sterben8912@gmail.com>
2025-03-27 16:15:35 +0500

    create main structure

--- PhotoMapAPI/Program.cs
+++ PhotoMapAPI/Program.cs
@@ -35,7 +35,6 @@ app.MapGet("/weatherforecast", () =>
 
 app.Run();
 
-// csosi
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
--- PhotoMapAPI/obj/Debug/net9.0/PhotoMapAPI.AssemblyInfo.cs
+++ PhotoMapAPI/obj/Debug/net9.0/PhotoMapAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhotoMapAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d21f8b0fbfbdb9cb6a70cf57b6e11ab58b733041")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d610bc946f00657d00b527695084040b9bca2c00")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhotoMapAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhotoMapAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
